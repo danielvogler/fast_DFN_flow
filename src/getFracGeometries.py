f997cd0747acbd5899f10bbb7eaaa75351b93edc
AH <ahobe@github.com>
2021-06-10 16:30:51 +0200

    Added initial pipeline ideas (not meant to be functional yet)

---
+++ src/getFracGeometries.py
@@ -0,0 +1,109 @@
+#! /usr/bin/python
+
+# Copyright:
+# 	Alex Hobe
+# 	Daniel Vogler
+# 	Martin P. Seybold
+
+
+# TODO
+# - extract abstractions?
+#	- import abstractions
+# - apeture value needed throughout.
+# 	- constant now, readable from file in a non-existent future.
+
+import sys # import sys.exit only?
+from xml.dom import minidom
+
+def abstractFracGeomGetter( fileName,fileType ):
+	""" Set of input methods, that extract fracture geometries from a file and convert this to the pipeline assumptions/abstraction.
+
+	"""
+
+def getFractureBoxes( abstractFracGeomGetter ):
+	# fileType as input, as multiple csv and xml types possible.
+	# use inheritence instead of hardcoded if/elif
+	if fileType == "GEOSxml":
+		#fracBoxes = readGEOSxml( fileName )
+		intCoordx, intCoordy, intCoordz = readGEOSxml( fileName )
+	elif fileType == "fracCSV":
+		print( "fracCSV reader not yet implemented" )
+		# TODO: error/exception instead of print
+		sys.exit()
+		fracBoxes = readFracCsv( fileName )
+	
+	# return fracBoxes
+	return intCoordx, intCoordy, intCoordz
+
+def abstractFracGeomReader( fileName ):
+	""" Set of readers, that extract fracture geometries from a file.
+
+		Usage: Input fileID, output fracture geometries
+	"""
+
+def readGEOSxml( abstractFracGeomReader ):
+	coordList = getCoordListGEOSxml( fileName )
+	#fracBoxes = makeFracBoxGEOSxml( coordList )
+	intCoordx, intCoordy, intCoordz = makeFracBoxGEOSxml( coordList )
+	# return fracBoxes
+	return intCoordx, intCoordy, intCoordz
+	
+def abstractCoordListGetter():
+	""" Set of converters between fracture-geometry-file specifics and fractureBox implementation.
+	"""
+
+def getCoordListGEOSxml( abstractCoordListGetter ):
+	xmldoc = minidom.parse( fileName ) 
+	coordList = xmldoc.getElementsByTagName( 'Nodeset' )
+	return coordList
+
+
+def abstractFracBoxMaker( coordList ):
+	""" Set of converters between fracture-geometry-file specifics and fractureBox implementation.
+	"""
+
+def makeFracBoxGEOSxml( abstractFracBoxMaker ):
+	# TODO: 
+	# - adjust to using box3D < not optimized for the problem?
+	# - provide aperture value elsewhere 
+	# 	- first needed here. Export as value of fracBoxes object?
+	# - adjust to fracBoxes first
+	# 	- fracBoxes.intCoordx etc.
+
+	# initialize coord minMax lists
+	intCoordx = []  
+    intCoordy = []  
+    intCoordz = []
+	aperture   = 1.0e-5
+	diff       = 0.01 - aperture # used to adjust for the arbitrary overlap to capture the nodes in GEOS.
+	nBoxes   = 0
+
+	for s in coordList:
+        x,y,z = s.attributes['xmin'].value.split()      # The minimum coordinates are extracted and 
+        intCoordx.append(float(x) + diff)                             # added to the coordinate lists
+        intCoordy.append(float(y) + diff)
+        intCoordz.append(float(z) + diff)        
+        x,y,z = s.attributes['xmax'].value.split()      # same for maximum coordinates
+        intCoordx.append(float(x) - diff)
+        intCoordy.append(float(y) - diff)
+        intCoordz.append(float(z) - diff)    
+        nBoxes  += 1
+
+	#return fracBoxes
+	return intCoordx, intCoordy, intCoordz
+
+# TODO:
+# - extend getFractureBoxes using inheritance here.
+
+def readFracCsv( abstractFracGeomReader ):
+	# TODO:
+	# - create desired csv
+	# - implement reader
+	coordList = getCoordListFracCsv( fileName )
+	fracBoxes = makeFracBoxesFracCsv( coordList )
+	return fracBoxes
+
+
+
+
+
