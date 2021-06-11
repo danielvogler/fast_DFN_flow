8b3fe926a51e25692f749992fcfc9d9b99b7d0a7
ahobe <ahobe@github.com>
2021-06-11 10:34:18 +0200

    Replaced reader part of HSPM_opt with tested functions.

--- src/getFracGeometries.py
+++ src/getFracGeometries.py
@@ -11,6 +11,7 @@
 #	- import abstractions
 # - apeture value needed throughout.
 # 	- constant now, readable from file in a non-existent future.
+# - produce inheritance?
 
 import sys # import sys.exit only?
 from xml.dom import minidom
@@ -20,12 +21,12 @@ def abstractFracGeomGetter( fileName,fileType ):
 
 	"""
 
-def getFractureBoxes( abstractFracGeomGetter ):
+def getFractureBoxes( fileName, fileType ):
 	# fileType as input, as multiple csv and xml types possible.
 	# use inheritence instead of hardcoded if/elif
 	if fileType == "GEOSxml":
 		#fracBoxes = readGEOSxml( fileName )
-		intCoordx, intCoordy, intCoordz = readGEOSxml( fileName )
+		intCoordx, intCoordy, intCoordz, nBoxes = readGEOSxml( fileName )
 	elif fileType == "fracCSV":
 		print( "fracCSV reader not yet implemented" )
 		# TODO: error/exception instead of print
@@ -33,7 +34,7 @@ def getFractureBoxes( abstractFracGeomGetter ):
 		fracBoxes = readFracCsv( fileName )
 	
 	# return fracBoxes
-	return intCoordx, intCoordy, intCoordz
+	return intCoordx, intCoordy, intCoordz, nBoxes
 
 def abstractFracGeomReader( fileName ):
 	""" Set of readers, that extract fracture geometries from a file.
@@ -41,28 +42,28 @@ def abstractFracGeomReader( fileName ):
 		Usage: Input fileID, output fracture geometries
 	"""
 
-def readGEOSxml( abstractFracGeomReader ):
+def readGEOSxml( fileName ):
 	coordList = getCoordListGEOSxml( fileName )
 	#fracBoxes = makeFracBoxGEOSxml( coordList )
-	intCoordx, intCoordy, intCoordz = makeFracBoxGEOSxml( coordList )
+	intCoordx, intCoordy, intCoordz, nBoxes = makeFracBoxGEOSxml( coordList )
 	# return fracBoxes
-	return intCoordx, intCoordy, intCoordz
+	return intCoordx, intCoordy, intCoordz, nBoxes
 	
-def abstractCoordListGetter():
+def abstractCoordListGetter( fileName ):
 	""" Set of converters between fracture-geometry-file specifics and fractureBox implementation.
 	"""
 
-def getCoordListGEOSxml( abstractCoordListGetter ):
-	xmldoc = minidom.parse( fileName ) 
-	coordList = xmldoc.getElementsByTagName( 'Nodeset' )
-	return coordList
+def getCoordListGEOSxml( fileName ):
+		xmldoc = minidom.parse( fileName ) 
+		coordList = xmldoc.getElementsByTagName( 'Nodeset' )
+		return coordList
 
 
 def abstractFracBoxMaker( coordList ):
 	""" Set of converters between fracture-geometry-file specifics and fractureBox implementation.
 	"""
 
-def makeFracBoxGEOSxml( abstractFracBoxMaker ):
+def makeFracBoxGEOSxml( coordList ):
 	# TODO: 
 	# - adjust to using box3D < not optimized for the problem?
 	# - provide aperture value elsewhere 
@@ -71,26 +72,26 @@ def makeFracBoxGEOSxml( abstractFracBoxMaker ):
 	# 	- fracBoxes.intCoordx etc.
 
 	# initialize coord minMax lists
-	intCoordx = []  
-    intCoordy = []  
-    intCoordz = []
+	intCoordx = []    
+	intCoordy = []    
+	intCoordz = []
 	aperture   = 1.0e-5
 	diff       = 0.01 - aperture # used to adjust for the arbitrary overlap to capture the nodes in GEOS.
 	nBoxes   = 0
 
 	for s in coordList:
-        x,y,z = s.attributes['xmin'].value.split()      # The minimum coordinates are extracted and 
-        intCoordx.append(float(x) + diff)                             # added to the coordinate lists
-        intCoordy.append(float(y) + diff)
-        intCoordz.append(float(z) + diff)        
-        x,y,z = s.attributes['xmax'].value.split()      # same for maximum coordinates
-        intCoordx.append(float(x) - diff)
-        intCoordy.append(float(y) - diff)
-        intCoordz.append(float(z) - diff)    
-        nBoxes  += 1
+		x,y,z = s.attributes['xmin'].value.split()      # The minimum coordinates are extracted and
+		intCoordx.append(float(x) + diff)				# added to the coordinate lists
+		intCoordy.append(float(y) + diff)
+		intCoordz.append(float(z) + diff)
+		x,y,z = s.attributes['xmax'].value.split()      # same for maximum coordinates
+		intCoordx.append(float(x) - diff)
+		intCoordy.append(float(y) - diff)
+		intCoordz.append(float(z) - diff)
+		nBoxes  += 1
 
 	#return fracBoxes
-	return intCoordx, intCoordy, intCoordz
+	return intCoordx, intCoordy, intCoordz, nBoxes
 
 # TODO:
 # - extend getFractureBoxes using inheritance here.
