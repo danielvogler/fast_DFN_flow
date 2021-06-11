9b877d24b710560e4b5bba8b745a079892243f73
AH <ahobe@github.com>
2021-06-11 11:06:10 +0200

    Added additional test for ISPM reader functions.

--- src/getFracGeometries.py
+++ src/getFracGeometries.py
@@ -15,6 +15,7 @@
 
 import sys # import sys.exit only?
 from xml.dom import minidom
+import numpy as np
 
 def abstractFracGeomGetter( fileName,fileType ):
 	""" Set of input methods, that extract fracture geometries from a file and convert this to the pipeline assumptions/abstraction.
@@ -92,6 +93,53 @@ def makeFracBoxGEOSxml( coordList ):
 
 	#return fracBoxes
 	return intCoordx, intCoordy, intCoordz, nBoxes
+
+def makeFracBoxGEOSxmlForISPM( coordList ):
+	# This differs slightly from HSPM version.
+	# Can we combine the two?
+    nBoxes   = 0
+    xCoord  = [[],[]] # fracture coordinates
+    yCoord  = [[],[]]
+    zCoord  = [[],[]]
+    verticesInBox = [] # lists which intersects lie on a fracture.
+    widthArray = []
+    longDimArray = []
+    dxArray = []
+    dyArray = []
+    dzArray = []
+    
+    for s in coordList:
+    # Here we run through each fracture (nodeset) in coordList.
+        x,y,z = s.attributes['xmin'].value.split()      # The minimum coordinates are extracted and 
+        xMin = float(x)
+        yMin = float(y)
+        zMin = float(z)
+        xCoord[0].append(xMin)
+        yCoord[0].append(yMin)
+        zCoord[0].append(zMin)
+        x,y,z = s.attributes['xmax'].value.split()      # same for maximum coordinates
+        xMax = float(x)
+        yMax = float(y)
+        zMax = float(z)
+        xCoord[1].append(xMax)
+        yCoord[1].append(yMax)
+        zCoord[1].append(zMax)
+        dx = xMax - xMin
+        dy = yMax - yMin
+        dz = zMax - zMin
+        vals = [dx,dy,dz]
+        index = np.argsort(vals) # sorts from smallest to largest and returns index
+        widthArray.append(vals[index[1]]) # second largest is the width
+        if   dx >= dy and dx >= dz:
+            lDim = 0
+        elif dy >= dx and dy >= dz:
+            lDim = 1
+        else:
+            lDim = 2
+        longDimArray.append(lDim)
+        verticesInBox.append( [] )    #init 
+        nBoxes  += 1
+    return xCoord, yCoord, zCoord, nBoxes, longDimArray, verticesInBox, widthArray
 
 # TODO:
 # - extend getFractureBoxes using inheritance here.
