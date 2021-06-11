1faddb199d3889a501560c12df629bf61edc833c
AH <ahobe@student.ethz.ch>
2021-06-11 07:55:59 +0200

    Created class to store all exception strings.

---
+++ src/ExceptionHandling.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+
+# Copyright:
+# 	Alex Hobe
+# 	Daniel Vogler
+# 	Martin P. Seybold
+
+
+# TODO
+# - Write exceptions here to be called when needed.
+
+class DFNexceptions():
+	""" DFNexceptions contains all exceptions.
+
+	"""
+	def readException():
+		return "Could not read file."
+		
